bc1ba6772b3a046f6431d01b36c414548b54688b
Xavier Quesada Allue <xavier@xqa.com.ar>
2011-04-19 18:41:27 +0200

    Add reset password ability

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ ActionController::Routing::Routes.draw do |map|
   map.login '/login', :controller => "user_sessions", :action => "new" 
   map.logout '/logout', :controller => "user_sessions", :action => "destroy"
 
+  # Be aware that many routes here are redundant because default routes are enabled! Tests will pass without this
   map.resources :users, :only => [ :new, :create, :edit, :update, :destroy ]
   map.resource :account, :controller => "users"
-
+  map.resources :reset_password_requests, :only => [ :new, :create, :edit, :update]
+  
   map.resources :organizations do |o|
     o.resources :projects, :only => [ :show, :new, :create, :edit, :update, :destroy]
     # o.resources :guest_team_memberships, :only => [ :new ] 
